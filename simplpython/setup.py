c4ae78be119a1da2c102f14673aa7879892c3477
Samthebest999 <69941412+Samthebest999@users.noreply.github.com>
2021-04-17 12:22:28 -0500

    Update setup.py

--- simplpython/setup.py
+++ simplpython/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='simplpython',
       version='0.1',
-      description='Easy Python | Python For Everybody',
+      description='simplpython | The simpler way of Pythoning',
       packages=['simplpython', 'gtts', 'playsound'],
       author_email='samit.mohnot@gmail.com',
       zip_safe=False)
