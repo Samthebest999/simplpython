9d5b6d0e34907b1f678c04697fb92a90171f17d7
Samthebest999 <samit.mohnot@gmail.com>
2021-04-17 12:52:44 -0500

    Update setup.py

--- simplpython/setup.py
+++ simplpython/setup.py
@@ -3,7 +3,7 @@ setup(
     name='simplpython',
     version='0.2',
     author = "Sam M",
-    long_description = "Go To [simplpython Github](
+    long_description = "Go To [simplpython Github](https://github.com/Samthebest999/simplpython)"
     description='simplpython | The simpler way of Pythoning',
     packages=['simplpython', 'gtts', 'playsound'],
     author_email='samit.mohnot@gmail.com',
