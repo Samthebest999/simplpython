45d7c43d60e3777c8ba4a911a03e231e21766320
Samthebest999 <samit.mohnot@gmail.com>
2021-04-17 12:10:35 -0500

    rename package

--- simplpython/setup.py
+++ simplpython/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
-setup(name='ezypython',
+setup(name='simplpython',
       version='0.1',
       description='Easy Python | Python For Everybody',
-      packages=['ezypython', 'gtts', 'playsound'],
+      packages=['simplepython', 'gtts', 'playsound'],
       author_email='samit.mohnot@gmail.com',
       zip_safe=False)
